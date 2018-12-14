df61d025b50d825165227f15fe85c6962241de64
xiuting.chen <xiuting.chen@100credit.com>
2018-12-14 17:00:05 +0800

    up

--- pages/IntegralMall/IntegralDetail/IntegralDetail.js
+++ pages/IntegralMall/IntegralDetail/IntegralDetail.js
@@ -10,6 +10,7 @@ Page({
    */
   data: {
     // MOCKDATA
+    showModalStatus: true,//是否展示付款弹窗
     detail:{
       images:'/images/home.png',
       describe: '面部护理',
@@ -17,15 +18,92 @@ Page({
       integral: ' 3020',
       stock: 99,
       imageList: ['/images/home.png', '/images/home.png', '/images/home.png', '/images/home.png', '/images/home.png', '/images/home.png', '/images/home.png',]
-    }
+    },
+
+    num: 1,
+    minusStatus: 'disable'
 
   },
 //  点击立即购买
   nowBuy:function(){
-    wx.navigateTo({
-      // url: '../../IntegralMall/IntegralDetail/IntegralDetail?id=' + id
-    })
+    this.showModal();
+  },
+  // 点击关闭弹层
+  closeModel:function(){
+    this.hideModal();
 
+  },
+  showModal: function () {
+    // 显示遮罩层
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation
+    animation.translateY(300).step()
+    this.setData({
+      animationData: animation.export(),
+      showModalStatus: true
+    })
+    setTimeout(function () {
+      animation.translateY(0).step()
+      this.setData({
+        animationData: animation.export()
+      })
+    }.bind(this), 200)
+  },
+  hideModal: function () {
+    // 隐藏遮罩层
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation
+    animation.translateY(300).step()
+    this.setData({
+      animationData: animation.export(),
+    })
+    setTimeout(function () {
+      animation.translateY(0).step()
+      this.setData({
+        animationData: animation.export(),
+        showModalStatus: false
+      })
+    }.bind(this), 200)
+  },
+
+  /*点击减号*/
+  bindMinus: function () {
+    var num = this.data.num;
+    if (num > 1) {
+      num--;
+    }
+    var minusStatus = num > 1 ? 'normal' : 'disable';
+    this.setData({
+      num: num,
+      minusStatus: minusStatus
+    })
+  },
+  /*点击加号*/
+  bindPlus: function () {
+    var num = this.data.num;
+    num++;
+    var minusStatus = num > 1 ? 'normal' : 'disable';
+    this.setData({
+      num: num,
+      minusStatus: minusStatus
+    })
+  },
+  /*输入框事件*/
+  bindManual: function (e) {
+    var num = e.detail.value;
+    var minusStatus = num > 1 ? 'normal' : 'disable';
+    this.setData({
+      num: num,
+      minusStatus: minusStatus
+    })
   },
   /**
    * 生命周期函数--监听页面加载
