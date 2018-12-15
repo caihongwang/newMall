ab060885868ba2a3c3085cb861b8d6ae6010718a
xiuting.chen <xiuting.chen@100credit.com>
2018-12-15 15:02:03 +0800

    up

--- pages/tabBar/my/my.js
+++ pages/tabBar/my/my.js
@@ -9,7 +9,34 @@ var app = getApp();
 
 Page({
   data: {
-   
+    tableData: [
+      {
+        "id": "1",
+        "name": "收货地址管理",
+        "page": "my/method/index",
+        "image":'/images/logo.png'
+      },
+      {
+        "id": "2",
+        "name": "积分订单列表",
+        "page": "my/howFind/index",
+        "image": '/images/logo.png'
+
+      },
+      {
+        "id": "3",
+        "name": "提现明细",
+        "page": "my/feedback/index",
+        "image": '/images/logo.png'
+      },
+      {
+        "id": "3",
+        "name": "提现明细",
+        "page": "my/feedback/index",
+        "image": '/images/logo.png',
+        'phone': '0102377455839' 
+      }
+    ],
     userInfo: {},
 
   }, 
