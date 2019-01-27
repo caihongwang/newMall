f1f16cd9d05d1db09f4d59f62c2a9e6a895ce195
caihongwang <caihongwang@dingtalk.com>
2019-01-27 14:01:46 +0800

    删除无用的包，减少主包大小

--- pages/tabBar/IntegralMall/IntegralMall.js
+++ pages/tabBar/IntegralMall/IntegralMall.js
@@ -11,58 +11,58 @@ Page({
    * 页面的初始数据
    */
   data: {
-    // MOCKDATA
-    screen: [],
+    productTitle: "",
     productList: [],
-    currentItem: 0,//当前选中的索引
+    productTypeList: [],
+    productTypeItem: 0 //当前选中商品类型的索引
   },
 
-
-  getProductList:function(e){
-    if(e){
-      var index = e.currentTarget.dataset.index;
-    }else{
-      var index = 0;
-    }
-    this.setData({
-      currentItem:index
-    })
+  //获取商品列表
+  getProductList: function() {
     var that = this;
+    console.log("this.data.productTypeItem = " + this.data.productTypeItem);
     var params = new Object();
     params.uid = wx.getStorageSync("UIDKEY");
-    params.category = this.data.screen[index].categoryCode;
-    network.POST(
-      {
-        params: params,
-        requestUrl: requestUrl.getProductListUrl,
-        success: function (res) {
-          console.log(res.data);
-          if (res.data.code == 0) {
-            console.log(res.data.data);
-            console.log(123);
-            console.log(res.data.data[0].describeImgUrl)
-            that.setData({
-              productList: res.data.data
-            })
+    params.category = this.data.productTypeList[this.data.productTypeItem].categoryCode;
+    if (this.data.productTitle){    //商品名称
+      params.title = this.data.productTitle;
+    }
+    network.POST({
+      params: params,
+      requestUrl: requestUrl.getProductListUrl,
+      success: function(res) {
+        console.log(res.data);
+        if (res.data.code == 0) {
+          that.setData({
+            productList: res.data.data
+          });
+        } else {
 
-          } else {
-
-          }
-
-        },
-        fail: function (res) {
-          util.toast("网络异常, 请稍后再试");
         }
-      });
+      },
+      fail: function(res) {
+        util.toast("网络异常, 请稍后再试");
+      }
+    });
+  },
 
-    //调用筛选接口
+  //绑定商品标题
+  bindProductTitleFunc: function(e) {
+    this.data.productTitle = e.detail.value;
+  },
 
-
+  //搜索商品
+  searchProduct: function(e) {
+    console.log("this.data.productTitle = " + this.data.productTitle);
+    if (this.data.productTitle) {
+      this.getProductList();
+    } else {
+      util.toast("请您输入要搜索商品标题.");
+    }
   },
 
   //点击跳转到详情页
-  getProductDetail:function(e){
-    console.log(e);
+  getProductDetail: function(e) {
     var productId = e.currentTarget.dataset.productid;
     console.log("productId = " + productId);
     wx.navigateTo({
@@ -70,9 +70,17 @@ Page({
     });
   },
 
+  //选择点击商品类型查询商品
+  selectProductType: function(e){
+    this.setData({
+      productTypeItem: e.currentTarget.dataset.producttypeitem,
+      productTitle: ""
+    });
+    this.getProductTypeList();
+  },
 
-  // 获取积分类型
-  getProductTypeList:function(boo){
+  // 获取商品类型列表
+  getProductTypeList: function(boo) {
     var that = this;
     var params = new Object();
     params.uid = wx.getStorageSync("UIDKEY");
@@ -83,86 +91,81 @@ Page({
         mask: true
       });
     }
-    network.POST(
-      {
-        params: params,
-        requestUrl: requestUrl.getProductTypeListUrl,
-        success: function (res) {
-          console.log(res.data);
-          boo ? wx.stopPullDownRefresh() : wx.hideLoading();
-          if (res.data.code == 0) {
-             console.log(res.data.data)
-             that.setData({
-               screen:res.data.data
-             })
-            that.getProductList();
-          } else {
+    network.POST({
+      params: params,
+      requestUrl: requestUrl.getProductTypeListUrl,
+      success: function(res) {
+        console.log(res.data);
+        boo ? wx.stopPullDownRefresh() : wx.hideLoading();
+        if (res.data.code == 0) {
+          that.setData({
+            productTypeList: res.data.data
+          });
+          that.getProductList();
+        } else {
 
-          }
-
-        },
-        fail: function (res) {
-          boo ? wx.stopPullDownRefresh() : wx.hideLoading();
-          util.toast("网络异常, 请稍后再试");
         }
-      });
-
-
+      },
+      fail: function(res) {
+        boo ? wx.stopPullDownRefresh() : wx.hideLoading();
+        util.toast("网络异常, 请稍后再试");
+      }
+    });
   },
+
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
     this.getProductTypeList(false);
-
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
