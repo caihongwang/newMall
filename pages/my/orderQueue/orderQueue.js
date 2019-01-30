2ee01c1e6dda507277d138d8aeacf964b71830b1
caihongwang <caihongwang@dingtalk.com>
2019-01-31 00:17:18 +0800

    优化 我的奖励队列或者奖励队列详情 样式

--- pages/my/orderQueue/orderQueue.js
+++ pages/my/orderQueue/orderQueue.js
@@ -9,27 +9,25 @@ Page({
    * 页面的初始数据
    */
   data: {
-
-
     havePageAll: 0, //已经加载的页数
-    pageindexAll: 10,//总共加载的总条数
-    howShops: 0,//共多少条列表数据
-    listLuck:[],//全部对列
-    myList:[], //我的对列
-    shopMap:{},
-
+    pageindexAll: 3, //总共加载的总条数
+    luckListTotal: 0, //共多少条列表数据
+    allList: [], //全部对列
+    myList: [], //我的对列
+    shopMap: {},
   },
-// 点击我的对列里的列表
-  goOrder:function(e){
+
+  // 点击我的对列里的列表
+  goOrder: function(e) {
     let that = this;
     let index = e.currentTarget.dataset.index;
     console.log(e.currentTarget.dataset.index);
-    if (this.data.listIndex == 2){
+    if (this.data.listIndex == 2) {
       return;
     }
-    if (that.data.myList[index].luckDrawStatusCode == 1){ //已经奖励
-       util.toast("已兑换奖励");
-    }else{
+    if (that.data.myList[index].luckDrawStatusCode == 1) { //已经奖励
+      util.toast("已兑换奖励");
+    } else {
       wx.showModal({
         title: '提示',
         content: '您确定把该订单换成积分吗？',
@@ -42,47 +40,42 @@ Page({
         }
       })
     }
-   
-
   },
 
-
-  convertIntegral: function (wxOrderId,wxAAppId) {
+  convertIntegral: function(wxOrderId, wxAAppId) {
     var that = this;
     var params = new Object();
     params.uid = wx.getStorageSync("UIDKEY");
     params.wxOrderId = wxOrderId;
-    network.POST(
-      {
-        params: params,
-        requestUrl: requestUrl.convertIntegralUrl,
-        success: function (res) {
-          if (res.data.code == 0) {
-            that.setData({
-              havePageAll: 0,
-              pageindexAll: 10
-            })
-            if (that.data.listIndex == 0){
-              that.getAllLuckDraw(false);
-            } else if (that.data.listIndex == 1){
-              that.getWaitLuckDraw(false)
-            }else{
-              that.getRecevicedLuckDraw(false)
-            }
+    network.POST({
+      params: params,
+      requestUrl: requestUrl.convertIntegralUrl,
+      success: function(res) {
+        if (res.data.code == 0) {
+          that.setData({
+            havePageAll: 0,
+            pageindexAll: 10,
+            allList: [], //全部对列
+            myList: [] //我的对列
+          });
+          if (that.data.listIndex == 0) {
+            that.getAllLuckDraw(false);
+          } else if (that.data.listIndex == 1) {
+            that.getWaitLuckDraw(false)
           } else {
-            util.toast(res.data.message);
+            that.getRecevicedLuckDraw(false)
           }
-
-        },
-        fail: function (res) {
-          util.toast("网络异常, 请稍后再试");
+        } else {
+          util.toast(res.data.message);
         }
-      });
+      },
+      fail: function(res) {
+        util.toast("网络异常, 请稍后再试");
+      }
+    });
   },
 
-
-
-  getAllLuckDraw: function (boo) {
+  getAllLuckDraw: function(boo) {
     var that = this;
     var params = new Object();
     params.uid = wx.getStorageSync("UIDKEY");
@@ -95,53 +88,54 @@ Page({
         mask: true
       });
     }
-    network.POST(
-      {
-        params: params,
-        requestUrl: requestUrl.getAllLuckDrawRankUrl,
-        success: function (res) {
-          boo ? wx.stopPullDownRefresh() : wx.hideLoading();
-          if (res.data.code == 0) {
-            res.data.data.all_allGetLuckDrawRankList = JSON.parse(res.data.data.all_allGetLuckDrawRankList);
-            res.data.data.my_allGetLuckDrawRankList = JSON.parse(res.data.data.my_allGetLuckDrawRankList);
-            res.data.data.shopMap = JSON.parse(res.data.data.shopMap);
-            for (var i in res.data.data.all_allGetLuckDrawRankList) {
-              that.data.listLuck.push(res.data.data.all_allGetLuckDrawRankList[i]);
-            }
-            that.setData({
-              listLuck: that.data.listLuck,
-              howShops: res.data.recordsFiltered,
-              myList: res.data.data.my_allGetLuckDrawRankList,
-              shopMap: res.data.data.shopMap
-            })
-            
-            that.data.havePageAll += res.data.data.length;
-            if (that.data.havePageAll < res.data.recordsFiltered) {
-              that.setData({
-                isShowMore: true,
-                loading: false,
-                isNoShowMore: false,
-              })
-            } else {
-              that.setData({
-                isShowMore: false,
-                loading: false,
-                isNoShowMore: true,
-              })
-            }
-            console.log(that.data.listLuck);
-          } else {
-            util.toast(res.data.message);
+    network.POST({
+      params: params,
+      requestUrl: requestUrl.getAllLuckDrawRankUrl,
+      success: function(res) {
+        boo ? wx.stopPullDownRefresh() : wx.hideLoading();
+        if (res.data.code == 0) {
+          console.log("请求之前 ");
+          console.log(that.data.allList);
+          res.data.data.all_allGetLuckDrawRankList = JSON.parse(res.data.data.all_allGetLuckDrawRankList);
+          res.data.data.my_allGetLuckDrawRankList = JSON.parse(res.data.data.my_allGetLuckDrawRankList);
+          res.data.data.shopMap = JSON.parse(res.data.data.shopMap);
+          for (var i in res.data.data.all_allGetLuckDrawRankList) {
+            that.data.allList.push(res.data.data.all_allGetLuckDrawRankList[i]);
           }
-
-        },
-        fail: function (res) {
-          boo ? wx.stopPullDownRefresh() : wx.hideLoading();
-          util.toast("网络异常, 请稍后再试");
+          that.setData({
+            allList: that.data.allList,
+            luckListTotal: res.data.recordsFiltered,
+            myList: res.data.data.my_allGetLuckDrawRankList,
+            shopMap: res.data.data.shopMap
+          }); 
+          that.data.havePageAll += res.data.data.all_allGetLuckDrawRankList.length;
+          if (that.data.havePageAll < res.data.data.length) {
+            that.setData({
+              isShowMore: true,
+              loading: false,
+              isNoShowMore: false,
+            });
+          } else {
+            that.setData({
+              isShowMore: false,
+              loading: false,
+              isNoShowMore: true,
+            })
+          }
+          console.log("请求之后 ");
+          console.log(that.data.allList);
+        } else {
+          util.toast(res.data.message);
         }
-      });
+      },
+      fail: function(res) {
+        boo ? wx.stopPullDownRefresh() : wx.hideLoading();
+        util.toast("网络异常, 请稍后再试");
+      }
+    });
   },
-  getWaitLuckDraw: function (boo) {
+
+  getWaitLuckDraw: function(boo) {
     var that = this;
     var params = new Object();
     params.uid = wx.getStorageSync("UIDKEY");
@@ -154,54 +148,53 @@ Page({
         mask: true
       });
     }
-    network.POST(
-      {
-        params: params,
-        requestUrl: requestUrl.getWaitLuckDrawRankUrl,
-        success: function (res) {
-          boo ? wx.stopPullDownRefresh() : wx.hideLoading();
-          if (res.data.code == 0) {
-            
-            res.data.data.all_waitGetLuckDrawRankList = JSON.parse(res.data.data.all_waitGetLuckDrawRankList);
-            res.data.data.my_waitGetLuckDrawRankList = JSON.parse(res.data.data.my_waitGetLuckDrawRankList);
-            res.data.data.shopMap = JSON.parse(res.data.data.shopMap);
-            for (var i in res.data.data.all_waitGetLuckDrawRankList) {
-              that.data.listLuck.push(res.data.data.all_waitGetLuckDrawRankList[i]);
-            }
-            that.setData({
-              listLuck: that.data.listLuck,
-              howShops: res.data.recordsFiltered,
-              myList: res.data.data.my_waitGetLuckDrawRankList,
-              shopMap: res.data.data.shopMap
-            })
+    network.POST({
+      params: params,
+      requestUrl: requestUrl.getWaitLuckDrawRankUrl,
+      success: function(res) {
+        boo ? wx.stopPullDownRefresh() : wx.hideLoading();
+        if (res.data.code == 0) {
 
-            that.data.havePageAll += res.data.data.length;
-            if (that.data.havePageAll < res.data.recordsFiltered) {
-              that.setData({
-                isShowMore: true,
-                loading: false,
-                isNoShowMore: false,
-              })
-            } else {
-              that.setData({
-                isShowMore: false,
-                loading: false,
-                isNoShowMore: true,
-              })
-            }
-            console.log(that.data.listLuck);
-          } else {
-            util.toast(res.data.message);
+          res.data.data.all_waitGetLuckDrawRankList = JSON.parse(res.data.data.all_waitGetLuckDrawRankList);
+          res.data.data.my_waitGetLuckDrawRankList = JSON.parse(res.data.data.my_waitGetLuckDrawRankList);
+          res.data.data.shopMap = JSON.parse(res.data.data.shopMap);
+          for (var i in res.data.data.all_waitGetLuckDrawRankList) {
+            that.data.allList.push(res.data.data.all_waitGetLuckDrawRankList[i]);
           }
-
-        },
-        fail: function (res) {
-          boo ? wx.stopPullDownRefresh() : wx.hideLoading();
-          util.toast("网络异常, 请稍后再试");
+          that.setData({
+            allList: that.data.allList,
+            luckListTotal: res.data.recordsFiltered,
+            myList: res.data.data.my_waitGetLuckDrawRankList,
+            shopMap: res.data.data.shopMap
+          });
+          that.data.havePageAll += res.data.data.all_waitGetLuckDrawRankList.length;
+          if (that.data.havePageAll < res.data.data.length) {
+            that.setData({
+              isShowMore: true,
+              loading: false,
+              isNoShowMore: false,
+            });
+          } else {
+            that.setData({
+              isShowMore: false,
+              loading: false,
+              isNoShowMore: true,
+            })
+          }
+          console.log(that.data.allList);
+        } else {
+          util.toast(res.data.message);
         }
-      });
+
+      },
+      fail: function(res) {
+        boo ? wx.stopPullDownRefresh() : wx.hideLoading();
+        util.toast("网络异常, 请稍后再试");
+      }
+    });
   },
-  getRecevicedLuckDraw: function (boo) {
+
+  getRecevicedLuckDraw: function(boo) {
     var that = this;
     var params = new Object();
     params.uid = wx.getStorageSync("UIDKEY");
@@ -214,62 +207,62 @@ Page({
         mask: true
       });
     }
-    network.POST(
-      {
-        params: params,
-        requestUrl: requestUrl.getRecevicedLuckDrawRankUrl,
-        success: function (res) {
-          boo ? wx.stopPullDownRefresh() : wx.hideLoading();
-          if (res.data.code == 0) {
+    network.POST({
+      params: params,
+      requestUrl: requestUrl.getRecevicedLuckDrawRankUrl,
+      success: function(res) {
+        boo ? wx.stopPullDownRefresh() : wx.hideLoading();
+        if (res.data.code == 0) {
 
-            res.data.data.all_recevicedGetLuckDrawRankList = JSON.parse(res.data.data.all_recevicedGetLuckDrawRankList);
-            res.data.data.my_recevicedGetLuckDrawRankList = JSON.parse(res.data.data.my_recevicedGetLuckDrawRankList);
-            res.data.data.shopMap = JSON.parse(res.data.data.shopMap);
-            for (var i in res.data.data.all_recevicedGetLuckDrawRankList) {
-              that.data.listLuck.push(res.data.data.all_recevicedGetLuckDrawRankList[i]);
-            }
-            that.setData({
-              listLuck: that.data.listLuck,
-              howShops: res.data.recordsFiltered,
-              myList: res.data.data.my_recevicedGetLuckDrawRankList,
-              shopMap: res.data.data.shopMap
-            })
-
-            that.data.havePageAll += res.data.data.length;
-            if (that.data.havePageAll < res.data.recordsFiltered) {
-              that.setData({
-                isShowMore: true,
-                loading: false,
-                isNoShowMore: false,
-              })
-            } else {
-              that.setData({
-                isShowMore: false,
-                loading: false,
-                isNoShowMore: true,
-              })
-            }
-            console.log(that.data.listLuck);
-          } else {
-            util.toast(res.data.message);
+          res.data.data.all_recevicedGetLuckDrawRankList = JSON.parse(res.data.data.all_recevicedGetLuckDrawRankList);
+          res.data.data.my_recevicedGetLuckDrawRankList = JSON.parse(res.data.data.my_recevicedGetLuckDrawRankList);
+          res.data.data.shopMap = JSON.parse(res.data.data.shopMap);
+          for (var i in res.data.data.all_recevicedGetLuckDrawRankList) {
+            that.data.allList.push(res.data.data.all_recevicedGetLuckDrawRankList[i]);
           }
-
-        },
-        fail: function (res) {
-          boo ? wx.stopPullDownRefresh() : wx.hideLoading();
-          util.toast("网络异常, 请稍后再试");
+          that.setData({
+            allList: that.data.allList,
+            luckListTotal: res.data.recordsFiltered,
+            myList: res.data.data.my_recevicedGetLuckDrawRankList,
+            shopMap: res.data.data.shopMap
+          });
+          that.data.havePageAll += res.data.data.all_recevicedGetLuckDrawRankList.length;
+          if (that.data.havePageAll < res.data.data.length) {
+            that.setData({
+              isShowMore: true,
+              loading: false,
+              isNoShowMore: false,
+            })
+          } else {
+            that.setData({
+              isShowMore: false,
+              loading: false,
+              isNoShowMore: true,
+            })
+          }
+          console.log(that.data.allList);
+        } else {
+          util.toast(res.data.message);
         }
-      });
+
+      },
+      fail: function(res) {
+        boo ? wx.stopPullDownRefresh() : wx.hideLoading();
+        util.toast("网络异常, 请稍后再试");
+      }
+    });
   },
 
-  bindMore: function () {
-    console.log(123123);
-    if (this.data.pageindexAll < this.data.howShops) {
-      this.setData({
+  bindMore: function() {
+    var that = this;
+    console.log("that.data.pageindexAll = " + that.data.pageindexAll);
+    console.log("that.data.luckListTotal = " + that.data.luckListTotal);
+    if (that.data.pageindexAll < that.data.luckListTotal) {
+      that.setData({
         loading: true,
         isShowMore: false,
         isNoShowMore: false
-      })
+      });
       if (that.data.listIndex == 0) {
         that.getAllLuckDraw(false);
       } else if (that.data.listIndex == 1) {
@@ -279,64 +272,70 @@ Page({
       }
     }
   },
+
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     console.log(options);
-    this.setData({
-      shopId: options.shopId,
-      listIndex: options.index
-    })
-    if (options.index == 0){
+    if (options.shopId) {
+      this.setData({
+        shopId: options.shopId
+      });
+    }
+    if (options.index) {
+      this.setData({
+        listIndex: options.index
+      });
+    }
+    if (options.index == 0) {
       this.getAllLuckDraw(false);
-    } else if (options.index == 1){
+    } else if (options.index == 1) {
       this.getWaitLuckDraw(false);
-    }else{
+    } else {
       this.getRecevicedLuckDraw(false)
     }
-
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
     this.bindMore();
 
   },
@@ -344,7 +343,7 @@ Page({
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
