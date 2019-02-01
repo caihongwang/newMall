f70eebbfefb3015ca741eb149347513e4971feb8
caihongwang <caihongwang@dingtalk.com>
2019-02-02 00:14:50 +0800

    优化 商家付款 的 各种输入框中的联动计算金额

--- pages/cardFile/payment/payment.js
+++ pages/cardFile/payment/payment.js
@@ -10,24 +10,16 @@ Page({
    */
   data: {
     chooseWechat: true, //是否选中了微信支付
-    payingBill: false,//是否选中了买单支付
-    isAgreement: false,//是否同意协议
-    useIntegralFlag:false,//是否积分抵现
-    useBalanceFlag: false,//是否余额抵现
-    integralInput:0,
-    payBalance:0,
+    payingBill: false, //是否选中了买单支付
+    isAgreement: false, //是否同意协议
+    useIntegralFlag: false, //是否积分抵现
+    useBalanceFlag: false, //是否余额抵现
+    integralOfDeduction: 0,
+    balanceOfDeduction: 0,
+    payMoney: "",
+    finalPayment: 0
   },
-  switchChange:function(e){
-    this.setData({
-      useIntegralFlag: e.detail.value
-    })
-  },
-  switchChange1: function (e) {
-    this.setData({
-      useBalanceFlag: e.detail.value
-    })
-  },
-  isAgree:function(){
+  isAgree: function() {
     // 点击是否选中图片
     this.data.isAgreement = !this.data.isAgreement
     this.setData({
@@ -35,19 +27,17 @@ Page({
     })
 
   },
-  weChatPay:function(){
-    if (!this.data.chooseWechat){
+  weChatPay: function() {
+    if (!this.data.chooseWechat) {
       this.setData({
         chooseWechat: true,
         payingBill: false,
-        useIntegralFlag:false,
-        useBalanceFlag:false
+        useIntegralFlag: false,
+        useBalanceFlag: false
       })
     }
-
- 
   },
-  selectLoan: function () {
+  selectLoan: function() {
     if (!this.data.payingBill) {
       this.setData({
         chooseWechat: false,
@@ -56,80 +46,176 @@ Page({
         useBalanceFlag: false
       })
     }
-   
-
-    
   },
 
-inputMoney:function(e){
-  this.setData({
-    payMoney: e.detail.value
-  })
-
-
-  },
-  interInput:function(e){
-    if (e.detail.value >= this.data.integral){
-      this.setData({
-        integralInput: this.data.integral
-      })
-    }else{
-      this.setData({
-        integralInput: e.detail.value
-      })
-    }
-  
-
-  },
-  balanceInput:function(e){
-    if (e.detail.value >= this.data.balance) {
-      this.setData({
-        payBalance: this.data.balance
-      })
-    } else {
-      this.setData({
-        payBalance: e.detail.value
-      })
-    }
-
-  },
   /**
-   * 生命周期函数--监听页面加载
+   * 打开积分switch
    */
+  switchIntegralChange: function(e) {
+    var useIntegralFlag = false;
+    if (e.detail.value) {
+      useIntegralFlag = e.detail.value;
+    }
+    var finalPayment = 0;
+    var integralOfDeduction = this.data.integralOfDeduction;
+    if (useIntegralFlag) {
+      finalPayment = this.data.payMoney - this.data.integralOfDeduction;
+      if (finalPayment < 0) {
+        finalPayment = 0;
+        integralOfDeduction = this.data.payMoney;
+      }
+    } else {
+      finalPayment = this.data.payMoney;
+    }
+    this.setData({
+      integralOfDeduction: integralOfDeduction,
+      useIntegralFlag: useIntegralFlag,
+      finalPayment: finalPayment
+    });
+  },
 
-  getUserBaseInfo: function () {
+  /**
+   * 打开余额switch
+   */
+  switchBanlanceChange1: function(e) {
+    var useBalanceFlag = false;
+    if (e.detail.value) {
+      useBalanceFlag = e.detail.value;
+    }
+    var finalPayment = 0;
+    var balanceOfDeduction = this.data.balanceOfDeduction;
+    if (useBalanceFlag) {
+      finalPayment = this.data.payMoney - this.data.balanceOfDeduction;
+      if (finalPayment < 0){
+        finalPayment = 0;
+        balanceOfDeduction = this.data.payMoney;
+      }
+    } else {
+      finalPayment = this.data.payMoney;
+    }
+    this.setData({
+      balanceOfDeduction: balanceOfDeduction,
+      useBalanceFlag: useBalanceFlag,
+      finalPayment: finalPayment
+    });
+  },
+
+  /**
+   * 付款金额-输入框监听
+   */
+  payMoneyInputFunc: function(e) {
+    var payMoney = 0;
+    if (e.detail.value + "") {
+      payMoney = e.detail.value;
+      var finalPayment = 0;
+      if (this.data.useIntegralFlag) {
+        finalPayment = payMoney - this.data.integralOfDeduction;
+      } else if (this.data.useIntegralFlag) {
+        finalPayment = payMoney - this.data.balanceOfDeduction;
+      } else {
+        finalPayment = payMoney;
+      }
+      this.setData({
+        payMoney: payMoney,
+        finalPayment: finalPayment
+      });
+    } else {
+      payMoney = 0;
+    }
+  },
+
+  /**
+   * 抵扣积分-输入框监听
+   */
+  integralInputFunc: function(e) {
+    var integralOfDeduction = 0;
+    if (e.detail.value + "") {
+      if (Number(e.detail.value) >= Number(this.data.integral)) {
+        integralOfDeduction = this.data.integral;
+      } else {
+        integralOfDeduction = e.detail.value;
+      }
+      var finalPayment = 0;
+      var payMoney = 0;
+      if (this.data.payMoney){
+        payMoney = this.data.payMoney;
+      }
+      if (this.data.useIntegralFlag) {
+        finalPayment = payMoney - integralOfDeduction;
+        if (finalPayment < 0) {
+          finalPayment = 0;
+          integralOfDeduction = this.data.payMoney;
+        }
+      } else {
+        finalPayment = payMoney;
+      }
+      this.setData({
+        integralOfDeduction: integralOfDeduction,
+        finalPayment: finalPayment
+      });
+    } else {
+      integralOfDeduction = 0;
+    }
+  },
+
+  /**
+   * 抵扣余额-输入框监听
+   */
+  balanceInputFunc: function(e) {
+    var balanceOfDeduction = 0;
+    if (e.detail.value + "") {
+      if (Number(e.detail.value) >= Number(this.data.balance)) {
+        balanceOfDeduction = this.data.balance;
+      } else {
+        balanceOfDeduction = e.detail.value;
+      }
+      var finalPayment = 0;
+      var payMoney = 0;
+      if (this.data.payMoney) {
+        payMoney = this.data.payMoney;
+      }
+      if (this.data.useBalanceFlag) {
+        finalPayment = payMoney - balanceOfDeduction;
+        if (finalPayment < 0) {
+          finalPayment = 0;
+          balanceOfDeduction = this.data.payMoney;
+        }
+      } else {
+        finalPayment = payMoney;
+      }
+      this.setData({
+        balanceOfDeduction: balanceOfDeduction,
+        finalPayment: finalPayment
+      });
+    } else {
+      balanceOfDeduction = 0;
+    }
+  },
+
+  getUserBaseInfo: function() {
     var that = this;
     var params = new Object();
     params.uid = wx.getStorageSync("UIDKEY");
-    network.POST(
-      {
-        params: params,
-        requestUrl: requestUrl.getUserBaseInfoUrl,
-        success: function (res) {
-          if (res.data.code == 0) {
-            that.setData({
-              balance: res.data.data.balance,
-              integral: res.data.data.integral,
-            })
-          } else {
-            util.toast(res.data.message);
-          }
-        },
-        fail: function (res) {
-          util.toast("网络异常, 请稍后再试");
+    network.POST({
+      params: params,
+      requestUrl: requestUrl.getUserBaseInfoUrl,
+      success: function(res) {
+        if (res.data.code == 0) {
+          that.setData({
+            balance: res.data.data.balance,
+            integral: res.data.data.integral,
+          })
+        } else {
+          util.toast(res.data.message);
         }
-      });
+      },
+      fail: function(res) {
+        util.toast("网络异常, 请稍后再试");
+      }
+    });
   },
 
-
-
-
-
-
-
-
-  
-  payTheBillInMiniUrl: function () {
+  payTheBillInMiniUrl: function() {
     var that = this;
     var params = new Object();
     params.uid = wx.getStorageSync("UIDKEY");
@@ -138,40 +224,38 @@ inputMoney:function(e){
     params.shopId = this.data.shopId;
     params.useBalanceFlag = this.data.useBalanceFlag;
     params.useIntegralFlag = this.data.useIntegralFlag;
-    params.payIntegral = this.data.integralInput;
-    params.payBalance = this.data.payBalance;
-    network.POST(
-      {
-        params: params,
-        requestUrl: requestUrl.payTheBillInMiniUrl,
-        success: function (res) {
-          if (res.data.code == 0) {
-            that.wxPayUnifiedOrder(res.data);
-          } else {
-            util.toast(res.data.message);
-          }
-        },
-        fail: function (res) {
-          util.toast("网络异常, 请稍后再试");
+    params.payIntegral = this.data.integralOfDeduction;
+    params.balanceOfDeduction = this.data.balanceOfDeduction;
+    network.POST({
+      params: params,
+      requestUrl: requestUrl.payTheBillInMiniUrl,
+      success: function(res) {
+        if (res.data.code == 0) {
+          that.wxPayUnifiedOrder(res.data);
+        } else {
+          util.toast(res.data.message);
         }
-      });
+      },
+      fail: function(res) {
+        util.toast("网络异常, 请稍后再试");
+      }
+    });
   },
 
-
-  wxPayUnifiedOrder: function (param) {        //点击付款/打赏，向微信服务器进行付款
+  wxPayUnifiedOrder: function(param) { //点击付款/打赏，向微信服务器进行付款
     //使用小程序发起微信支付
     wx.requestPayment({
-      timeStamp: param.data.timeStamp,//记住，这边的timeStamp一定要是字符串类型的，不然会报错，我这边在java后端包装成了字符串类型了
+      timeStamp: param.data.timeStamp, //记住，这边的timeStamp一定要是字符串类型的，不然会报错，我这边在java后端包装成了字符串类型了
       nonceStr: param.data.nonceStr,
       package: param.data.package,
-      signType: 'MD5',      //小程序发起微信支付，暂时只支持“MD5”
+      signType: 'MD5', //小程序发起微信支付，暂时只支持“MD5”
       paySign: param.data.paySign,
-      success: function (event) {
-        wx.showToast({              //支付成功
+      success: function(event) {
+        wx.showToast({ //支付成功
           title: '支付成功',
           icon: 'success',
           duration: 2000,
-          complete: function () {   //支付成功后发送模板消息
+          complete: function() { //支付成功后发送模板消息
             console.log("模板消息已发送");
             let wxOrderId = param.data.wxOrderId;
             wx.navigateTo({
@@ -180,7 +264,7 @@ inputMoney:function(e){
           }
         });
       },
-      fail: function (error) {      //支付失败
+      fail: function(error) { //支付失败
         console.log("支付失败");
         console.log(error);
         wx.showModal({
@@ -189,19 +273,20 @@ inputMoney:function(e){
           showCancel: false
         });
       },
-      complete: function () {       //不管支付成功或者失败之后都要处理的方法，类似与final
+      complete: function() { //不管支付成功或者失败之后都要处理的方法，类似与final
         console.log("支付完成");
       }
     });
   },
 
-
-
-  surePay:function(){
-   this. payTheBillInMiniUrl();
+  surePay: function() {
+    this.payTheBillInMiniUrl();
   },
 
-  onLoad: function (options) {
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
     var shopId = "";
     var shopTitle = "";
     if (options.shopId) {
@@ -224,14 +309,14 @@ inputMoney:function(e){
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
     this.getUserBaseInfo();
 
   },
@@ -239,38 +324,35 @@ inputMoney:function(e){
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
-
-
-
